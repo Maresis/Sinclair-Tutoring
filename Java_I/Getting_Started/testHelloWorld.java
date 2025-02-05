a72c5a9a46f40ba3a0ba430d962222053f50b8a4
Maresis <mohunt9521@gmail.com>
2025-02-05 09:43:12 -0500

    Created Getting Started

---
+++ Java_I/Getting_Started/testHelloWorld.java
@@ -0,0 +1,17 @@
+package Java_I.Getting_Started; //visual studio HATES custom packages lol, but they are necessary to run your programs
+//put the name of the package of your program (the folder its in) on the first line to make sure it runs correctly.
+/**
+ * This class simply prints out the statement "hello world"
+ * This is used to test your ability to run and debug a program in Java.
+ * 
+ * Instructions:
+ * Lets get started with making sure you can run these example projects in visual studio code.
+ * 1.The first thing you need to do is make sure you have the appropriate extensions in visual studio
+ * code. I use Java (https://marketplace.visualstudio.com/items?itemName=Oracle.oracle-java) and 
+ */
+public class testHelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
